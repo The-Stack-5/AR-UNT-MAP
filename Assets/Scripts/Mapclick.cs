781f34c40c14f45befaef1c01bd098d7f43d2941
vickyw220 <vickywainaina@my.unt.edu>
2026-02-12 02:47:47 -0600

    Add RoomInfo3D integration to MapClick update
    Integrated RoomInfo3D to show and hide room info on click.

--- Assets/Scripts/Mapclick.cs
+++ Assets/Scripts/Mapclick.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class MapClick : MonoBehaviour
 {
@@ -25,10 +25,26 @@ public class MapClick : MonoBehaviour
         {
             if (wall != null && wall.OverlapPoint(clickPos))
             {
-                // Valid click → move marker + notify navigator
+                // Move the marker
                 YouAreHere.transform.position = clickPos;
                 Debug.Log("MapClick: YouAreHere moved to " + clickPos);
 
+                // --- RoomInfo3D integration ---
+                RoomInfo3D roomInfo = wall.GetComponent<RoomInfo3D>();
+                if (roomInfo != null)
+                {
+                    // Show info for this room
+                    roomInfo.ShowInfo();
+
+                    // Hide info for all other rooms
+                    foreach (var r in FindObjectsOfType<RoomInfo3D>())
+                    {
+                        if (r != roomInfo)
+                            r.HideInfo();
+                    }
+                }
+
+                // Notify navigator if you have one
                 MapNavigator nav = GetComponent<MapNavigator>();
                 if (nav != null)
                 {
